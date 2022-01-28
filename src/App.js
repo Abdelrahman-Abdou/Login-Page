1a18de46a6e15c0a00370ad033cb4dea2643ec0a
Abdelrahman Mohamed <79381329+Abdelrahman-ctr@users.noreply.github.com>
2022-01-29 00:03:19 +0200

    Managing Effects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,23 +6,28 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  useEffect(() => {
+    const userLoginInfo = localStorage.getItem('loggedIn')
+    if (userLoginInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    localStorage.setItem('loggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('loggedIn','1')
     setIsLoggedIn(false);
   };
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader isAuthenticated={ isLoggedIn } onLogout={ logoutHandler } />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        { !isLoggedIn && <Login onLogin={ loginHandler } /> }
+        { isLoggedIn && <Home onLogout={ logoutHandler } /> }
       </main>
     </React.Fragment>
   );
